9426e6366d82ea6a3d1474140a2cbec26b2cdf9b
Henrique Rodrigues <henrique.rodrigues@globalsys.com.br>
2024-11-26 12:04:03 -0300

    Add css layer to use tailwind classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<AntdRegistry>
+				<AntdRegistry layer>
 					<App>{children}</App>
 				</AntdRegistry>
 			</body>
