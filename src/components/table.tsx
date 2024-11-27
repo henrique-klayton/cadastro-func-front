b69c6d33e643c655ab6f2038d8a39589134fa755
Henrique Rodrigues <henrique.rodrigues@globalsys.com.br>
2024-11-27 11:06:25 -0300

    Improve table typing and add key prop to columns

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -3,24 +3,27 @@ import { Table } from "antd";
 import { ColumnProps } from "antd/es/table";
 
 export type TableColumn<T> = ColumnProps<T> & RequiredColumnProps<T>;
-export type RequiredColumnProps<T> = Required<
-	Pick<ColumnProps<T>, "title" | "dataIndex">
->;
+export interface RequiredColumnProps<T > {
+	title: string;
+	dataIndex: Extract<keyof T, string | number>;
+}
 
-export interface ListingTableProps<T extends { [key: string]: unknown }> {
+export interface ListingTableProps<T> {
 	data: T[];
 	rowKey: keyof T;
 	columns: Array<TableColumn<T>>;
 }
 
-export default function ListingTable<T extends { [key: string]: unknown }>({
+export default function ListingTable<T extends object>({
 	data,
 	rowKey,
 	columns,
 }: ListingTableProps<T>) {
 	const tableColumns = [];
 	for (const item of columns) {
-		tableColumns.push(<Table.Column {...item} />);
+		tableColumns.push(
+			<Table.Column {...item} key={item.key ?? item.dataIndex} />,
+		);
 	}
 
 	return (
