f547307df37fe8618cbd24b16c1606d92832dd13
Henrique Rodrigues <henrique.rodrigues@globalsys.com.br>
2024-11-27 15:54:43 -0300

    WIP Actions column for edit and delete buttons

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -1,7 +1,16 @@
 "use client";
-import { Table, Tag } from "antd";
+import { Button, Flex, Table, Tag, Tooltip } from "antd";
 import { ColumnProps } from "antd/es/table";
 import { ReactNode } from "react";
+import { AiOutlineDelete, AiOutlineForm } from "react-icons/ai";
+
+export interface DataTableActions<T> {
+	create?: (data: T) => Promise<unknown>;
+	update?: (data: T) => Promise<unknown>;
+	delete?: (data: T) => Promise<unknown>;
+	onUpdateClick: () => unknown;
+	onDeleteClick: () => unknown;
+}
 
 export type TableColumn<T> = ColumnProps<T> & RequiredColumnProps<T>;
 export interface RequiredColumnProps<T> {
@@ -13,6 +22,7 @@ export interface DataTableProps<T> {
 	data: T[];
 	rowKey: keyof T;
 	columns: Array<TableColumn<T>>;
+	actions?: DataTableActions<T>;
 }
 
 const renderStatus = (_: unknown, { status }: { status: boolean }) => {
@@ -24,6 +34,7 @@ export default function DataTable<T extends object>({
 	data,
 	rowKey,
 	columns: columnsProps,
+	actions,
 }: DataTableProps<T>) {
 	const columns = columnsProps.map((item) => {
 		if (item.dataIndex === "status")
@@ -31,6 +42,44 @@ export default function DataTable<T extends object>({
 		if (item.align != null) item.align = "center";
 		return <Table.Column {...item} key={item.key ?? item.dataIndex} />;
 	});
+
+	const renderActions = () => {
+		return (
+			<>
+				<Flex
+					justify="flex-start"
+					align="center"
+					gap="0.5rem"
+					// className="text-2xl"
+				>
+					<Tooltip title="Editar">
+						<Button
+							icon={<AiOutlineForm />}
+							size="large"
+							color="primary"
+							variant="outlined"
+							// className="text-blue-600"
+							// onClick={actions.onUpdateClick}
+						/>
+					</Tooltip>
+					<Tooltip title="Remover">
+						<Button
+							danger
+							icon={<AiOutlineDelete />}
+							size="large"
+							variant="outlined"
+							// className="text-red-600"
+							// onClick={actions.onDeleteClick}
+						/>
+					</Tooltip>
+				</Flex>
+			</>
+		);
+	};
+
+	columns.push(
+		<Table.Column title="Ações" key="actions" render={renderActions} />,
+	);
 
 	return (
 		<Table<T> dataSource={data} rowKey={rowKey}>
