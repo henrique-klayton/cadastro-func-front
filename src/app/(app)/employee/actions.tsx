11bd113ebe2db49a6002bd57570ed89e4fcb067c
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2024-11-28 15:18:20 -0300

    Add query to get employees with relations included

--- src/app/(app)/employee/actions.tsx
+++ src/app/(app)/employee/actions.tsx
@@ -1,13 +1,18 @@
 "use server";
-import Employee, { EmployeeType } from "@fragments/employee";
+import {
+	Employee,
+	EmployeeType,
+	FullEmployee,
+	FullEmployeeType,
+} from "@fragments/employee";
 import { graphql } from "@graphql-types/gql";
 import { EmployeeCreateDto } from "@graphql-types/graphql";
 import getUrqlClient from "@graphql/client";
 
 const getFullEmployeeQuery = graphql(`
 	query GetFullEmployee($id: ID!) {
-		employee(id: $id) {
-			...Employee
+		employeeWithRelations(id: $id) {
+			...FullEmployee
 		}
 	}
 `).toString();
@@ -43,6 +48,18 @@ const deleteEmployeeMutation = graphql(`
 		}
 	}
 `).toString();
+
+export async function getEmployeeWithRelations(
+	id: string,
+): Promise<FullEmployeeType> {
+	const { data: queryResult } = await getUrqlClient()
+		.query(getFullEmployeeQuery, { id })
+		.toPromise();
+	if (queryResult?.employeeWithRelations != null) {
+		return FullEmployee(queryResult.employeeWithRelations);
+	}
+	throw new Error("Error getting employee with relations");
+}
 
 export async function getEmployees(
 	filterStatus = false,
