b70ab0ed87d0d89eb18e4be8bd9c12b75459bc22
Henrique Rodrigues <henrique.rodrigues@globalsys.com.br>
2024-11-29 10:38:56 -0300

    Move queries and mutation to a separate file

--- src/app/(app)/employee/actions.tsx
+++ src/app/(app)/employee/actions.tsx
@@ -5,49 +5,13 @@ import {
 	FullEmployee,
 	FullEmployeeType,
 } from "@fragments/employee";
-import { graphql } from "@graphql-types/gql";
 import { EmployeeCreateDto } from "@graphql-types/graphql";
 import getUrqlClient from "@graphql/client";
-
-const getFullEmployeeQuery = graphql(`
-	query GetFullEmployee($id: ID!) {
-		employeeWithRelations(id: $id) {
-			...FullEmployee
-		}
-	}
-`).toString();
-
-const getEmployeesListQuery = graphql(`
-	query GetEmployees($filterStatus: Boolean) {
-		employeeList(filterStatus: $filterStatus) {
-			...Employee
-		}
-	}
-`).toString();
-
-const createEmployeeMutation = graphql(`
-	mutation CreateEmployee($employee: EmployeeCreateDto!) {
-		createEmployee(employee: $employee) {
-			...Employee
-		}
-	}
-`).toString();
-
-const updateEmployeeMutation = graphql(`
-	mutation UpdateEmployee($id: ID!, $employee: EmployeeUpdateDto!) {
-		updateEmployee(id: $id, employee: $employee) {
-			...Employee
-		}
-	}
-`).toString();
-
-const deleteEmployeeMutation = graphql(`
-	mutation DeleteEmployee($id: ID!) {
-		deleteEmployee(id: $id) {
-			...Employee
-		}
-	}
-`).toString();
+import {
+	createEmployeeMutation,
+	getEmployeesListQuery,
+	getFullEmployeeQuery,
+} from "@queries/employee";
 
 export async function getEmployeeWithRelations(
 	id: string,
