b00a277df0c9b2dd1da2650f4d6edc8c0b189cef
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2024-12-10 11:41:36 -0300

    Improve FormModal and TablePage types

--- src/components/table-page/types.ts
+++ src/components/table-page/types.ts
@@ -1,39 +1,22 @@
 import { DataTableProps } from "@components/data-table/types";
 import {
-	FormCreateData,
 	FormData,
-	FormUpdateData,
+	FormModalCreateProps,
+	FormModalUpdateProps,
+	MergedFormModalProps,
 } from "@components/form-modal/types";
-import { ActionsEnum } from "@enums/actions";
 import { HaveId } from "@interfaces/have-id";
 import { Optional } from "@interfaces/optional.type";
-import { FormInstance } from "antd/lib";
 
 // biome-ignore lint/suspicious/noExplicitAny: Impossible to know formatter return value beforehand
 export type QueryDataParsers<T> = { [P in keyof T]: (value: T[P]) => any };
 export type FormDataSerializer<T> = (data: FormData<T>) => T;
 
-export type FormModalStateProps<C, U> =
-	| FormModalCreateStateProps<C>
-	| FormModalUpdateStateProps<U>;
+export type TablePageFormModalProps<C, U> =
+	| FormModalCreateProps<C>
+	| FormModalUpdateProps<U>;
 
-export interface FormModalCreateStateProps<C> {
-	action: ActionsEnum.CREATE;
-	form: FormInstance<C>;
-	initialData: C | undefined;
-	currentId: undefined;
-	onSubmit: (submit: FormCreateData<C>) => void;
-	// onFieldsChange: (changedFields: unknown[], allFields: unknown[]) => void;
-}
-
-export interface FormModalUpdateStateProps<U> {
-	action: ActionsEnum.UPDATE;
-	form: FormInstance<U>;
-	initialData: U | undefined;
-	currentId: string | number;
-	onSubmit: (submit: FormUpdateData<U>) => void;
-	// onFieldsChange: (changedFields: unknown[], allFields: unknown[]) => void;
-}
+export type FormModalStateProps<C, U> = MergedFormModalProps<C, U>;
 
 export interface TablePageProps<T extends HaveId, C extends U, U> {
 	children: React.ReactNode;
