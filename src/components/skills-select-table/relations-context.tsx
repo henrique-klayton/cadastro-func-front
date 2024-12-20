948e9be101d8ed0ea592ca9c8d4a476390b03c41
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2024-12-20 10:52:49 -0300

    Create context for relation tables reducer

---
+++ src/components/skills-select-table/relations-context.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { ItemRelationsAction } from "@components/table-page/interfaces/Item-relations-action";
+import {
+	ItemRelationObject,
+	RelationKeyObject,
+} from "@components/table-page/types";
+import StringKeyof from "@interfaces/string-keyof.type";
+import { createContext, useContext } from "react";
+
+export interface RelationsTablesProviderProps<T> {
+	children: React.ReactNode;
+	relationsKeys?: Array<RelationKeyObject<T>>;
+}
+
+export const RelationsContext = createContext(
+	{} as ItemRelationObject<unknown>,
+);
+export const RelationsDispatchContext = createContext(
+	null as unknown as React.Dispatch<ItemRelationsAction<unknown>>,
+);
+
+export const createRelationsContext = <T,>() =>
+	RelationsContext as React.Context<ItemRelationObject<T>>;
+export const createRelationsDispatchContext = <T,>() =>
+	RelationsDispatchContext as unknown as React.Context<
+		React.Dispatch<ItemRelationsAction<T>>
+	>;
+
+export function useRelations() {
+	return useContext(RelationsContext);
+}
+
+export function useRelation<T>(key: StringKeyof<T>) {
+	const relations = useContext(RelationsContext) as ItemRelationObject<T>;
+	return relations[key];
+}
+
+export function useRelationsDispatch() {
+	return useContext(RelationsDispatchContext);
+}
