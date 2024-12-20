9f0f91664599a5bad76602f061cba1d3bd079939
Henrique Rodrigues <henrique.rodrigues@globalsys.com.br>
2024-12-20 15:52:31 -0300

    Create lazy-loaded tables using Next.js dynamic

--- src/components/skills-select-table/skills-select-table.tsx
+++ src/components/skills-select-table/skills-select-table.tsx
@@ -1,10 +1,26 @@
 "use client";
 import { Table } from "antd";
 import { TableRowSelection } from "antd/es/table/interface";
+import dynamic from "next/dynamic";
+
 import Flatten from "@interfaces/flatten.type";
 import StringKeyof from "@interfaces/string-keyof.type";
 import { useRelation } from "./relations-context";
 import { SkillsSelectTableProps } from "./types";
+
+export const DynamicTable = dynamic(
+	import("@components/skills-select-table/skills-select-table"),
+	{
+		ssr: false,
+		loading: () => <Table loading={true} />,
+	},
+) as React.ComponentType<SkillsSelectTableProps<unknown>>;
+
+export const createRelationsTable = <T,>(
+	dataKey: StringKeyof<T>,
+): React.ReactElement<SkillsSelectTableProps<T>> => {
+	return <DynamicTable dataKey={dataKey as StringKeyof<unknown>} />;
+};
 
 export default function SkillsSelectTable<T>({
 	dataKey,
