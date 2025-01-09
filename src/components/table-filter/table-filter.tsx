b6735058f6a0b1c4c9ac31d3309d30b1e14d0930
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2025-01-09 11:38:28 -0300

    Imrpove table filter style and use props Col span

--- src/components/table-filter/table-filter.tsx
+++ src/components/table-filter/table-filter.tsx
@@ -1,5 +1,5 @@
 import StringKeyof from "@interfaces/string-keyof.type";
-import { Col, Form, Row, Select } from "antd";
+import { Col, Form, Row, Select, Typography } from "antd";
 import TableFilterProps from "./table-filter-props";
 
 export default function TableFilterComponent<T>({
@@ -7,6 +7,7 @@ export default function TableFilterComponent<T>({
 	tableData,
 	onFilterChange,
 }: TableFilterProps<T>) {
+	const { Title } = Typography;
 	const filters: React.ReactNode[] = Object.keys(filtersProps).map(
 		(stringKey) => {
 			const key = stringKey as StringKeyof<T>;
@@ -23,7 +24,7 @@ export default function TableFilterComponent<T>({
 				);
 			};
 			return (
-				<Col key={props.name} span={4}>
+				<Col key={props.name} span={props.colSpan}>
 					<Form.Item label={props.label} name={props.name as string}>
 						<Select
 							defaultValue={props.initialValue}
@@ -38,7 +39,14 @@ export default function TableFilterComponent<T>({
 
 	return (
 		<Form layout="vertical">
-			<Row gutter={24}>{filters}</Row>
+			<Row gutter={24} justify="end" align="middle">
+				<Col span={2}>
+					<Title className="text-end" level={5}>
+						Filtros:
+					</Title>
+				</Col>
+				{filters}
+			</Row>
 		</Form>
 	);
 }
